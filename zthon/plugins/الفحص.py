12421de789cdecf815045f7e77bc58d158977a21
TOPTH0N <119265616+TOPTH0N@users.noreply.github.com>
2023-07-23 03:35:40 +0300

    Update الفحص.py

--- zthon/plugins/الفحص.py
+++ zthon/plugins/الفحص.py
@@ -36,7 +36,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "✥┊"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت السيد 𝔸𝕃𝕊𝕀𝔻 يعمـل .. بنجـاح ☑️ 𓆩 **"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت السيد Syntral يعمـل .. بنجـاح ☑️ 𓆩 **"
     ZED_IMG = gvarstatus("ALIVE_PIC") or "https://graph.org/file/64ae7877acd791b791354.mp4"
     zed_caption = gvarstatus("ALIVE_TEMPLATE") or zed_temp
     caption = zed_caption.format(
@@ -95,12 +95,11 @@ async def amireallyialive(event):
     "A kind of showing bot details by your inline bot"
     reply_to_id = await reply_id(event)
     Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "✥┊"
-    zed_caption = "** بـوت السيد 𝔸𝕃𝕊𝕀𝔻 يعمـل .. بنجـاح ☑️ 𓆩 **\n"
+    zed_caption = "** بـوت السيد Syntral يعمـل .. بنجـاح ☑️ 𓆩 **\n"
     zed_caption += f"**{Z_EMOJI} إصـدار التـيليثون :** `{version.__version__}\n`"
     zed_caption += f"**{Z_EMOJI} إصـدار السيد :** `{repversion}`\n"
     zed_caption += f"**{Z_EMOJI} إصـدار البـايثون :** `{python_version()}\n`"
     zed_caption += f"**{Z_EMOJI} المسـتخدم :** {mention}\n"
-    zed_caption += f"**{Z_EMOJI} المطورين ~ @S_i_D ٬ @zqqqzq
     results = await event.client.inline_query(Config.TG_BOT_USERNAME, zed_caption)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
@@ -110,3 +109,4 @@ async def amireallyialive(event):
 async def on_plug_in_callback_query_handler(event):
     statstext = await zedalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
+    
